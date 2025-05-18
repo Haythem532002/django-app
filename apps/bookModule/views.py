c96b0daa3ac413359f44973a195df345d8bb5f22
Haythem532002 <khiarih06@gmail.com>
2025-05-18 23:32:36 +0100

    add crud for book

--- apps/bookModule/views.py
+++ apps/bookModule/views.py
@@ -1,4 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Book
+from .forms import BookForm
 
-def index(request):
-    return render(request, "bookModule/index.html")
+# LIST (Read All)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookModule/book_list.html', {'books': books})
+
+# CREATE
+def book_create(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'bookModule/book_form.html', {'form': form})
+
+# UPDATE
+def book_update(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'bookModule/book_form.html', {'form': form})
+
+# DELETE
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'bookModule/book_confirm_delete.html', {'book': book})
